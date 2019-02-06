8aefb257ba62fe7179aa7f2a958a283e167796b1
Rashid Lasker <rashidlasker@gmail.com>
2019-02-06 02:13:26 -0500

    make get request functionality

--- app/microservices/rebu/views.py
+++ app/microservices/rebu/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
+from rebu.models import user
+from django.forms.models import model_to_dict
 
 # Create your views here.
+def users(request, id=-1):
+    if request.method == 'GET':
+        try:
+            obj = user.objects.get(pk=id)
+            return JsonResponse(model_to_dict(obj))
+        except:
+            return HttpResponseBadRequest()
+    elif request.method == 'POST':
+        return JsonResponse({'foo':id})
+    elif request.method == 'DELETE':
+        return JsonResponse({'foo':id})
+
+def create_user(request):
+    return JsonResponse({'foo':'bar'})
