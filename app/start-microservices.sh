0d0f8f700c270e230e5c1594c75c51e6bba52f93
Rashid Lasker <rashidlasker@gmail.com>
2019-02-13 12:31:20 -0500

    reorganize startup commands

---
+++ app/start-microservices.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python microservices/manage.py migrate
+python microservices/manage.py loaddata microservices/rebu/fixtures/rebu/rebu_testdata.json 
+mod_wsgi-express start-server --working-directory microservices/ --reload-on-changes microservices/microservices/wsgi.py
