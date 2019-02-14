12eedf93a38e770a0f4d6e6af8de07f350e09153
Rashid Lasker <rashidlasker@gmail.com>
2019-02-14 18:57:31 -0500

    move microservice start script

--- app/start-microservices.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python microservices/manage.py migrate
-python microservices/manage.py flush --no-input
-python microservices/manage.py loaddata microservices/rebu/fixtures/rebu/rebu_testdata.json 
-mod_wsgi-express start-server --working-directory microservices/ --reload-on-changes microservices/microservices/wsgi.py
