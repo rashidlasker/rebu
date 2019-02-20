93ef493f57d804266f4031416960ed4416ad6d12
Rashid Lasker <rashidlasker@gmail.com>
2019-02-20 09:37:11 -0500

    refactor urls

--- app/web/web/urls.py
+++ app/web/web/urls.py
@@ -11,14 +11,12 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  ​path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import include, path
 
-from pages import views as pageviews
-
 urlpatterns = [
-	path('', pageviews.index, name='index'),
+	path('', include('pages.urls')),
     path('admin/', admin.site.urls),   
 ]
