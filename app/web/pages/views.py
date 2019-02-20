2756c50ec020accbcb867e3b72fa671308b81b71
Rashid Lasker <rashidlasker@gmail.com>
2019-02-20 18:25:00 -0500

    modularize homepage

--- app/web/pages/views.py
+++ app/web/pages/views.py
@@ -3,5 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	context = {}
+	context = {
+		"newest_meals": [
+			{
+				"name": "Pizza",
+				"description": "its food!",
+				"calories": 600,
+				"tags": ["yum", "yummy", "yum"]
+			},
+			{
+				"name": "Pizza2",
+				"description": "its food2!",
+				"calories": 700,
+				"tags": ["yum2", "yummy", "yum"]
+			},
+			{
+				"name": "Pizza3",
+				"description": "its food3!",
+				"calories": 800,
+				"tags": ["yum3", "yummy", "yum"]
+			},
+		]
+	}
 	return render(request, 'pages/index.html', context)
+
