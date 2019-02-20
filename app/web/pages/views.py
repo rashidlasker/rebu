c99cd141b08b9ec6887819e329b400313100d974
Rashid Lasker <rashidlasker@gmail.com>
2019-02-20 11:31:28 -0500

    import static assets

--- app/web/pages/views.py
+++ app/web/pages/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("aye lmao")
-	# template = loader.get_template('polls/index.html')
-	# context = {}
- #    return render(request, 'polls/index.html', context)
+	context = {}
+	return render(request, 'pages/index.html', context)
