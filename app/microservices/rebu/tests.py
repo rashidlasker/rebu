ded0ab7f2b014259e16791e5d7c7cdca70cfee0b
awh4kc <awh4kc@virginia.edu>
2019-02-20 00:22:25 -0500

    basic tests working

--- app/microservices/rebu/tests.py
+++ app/microservices/rebu/tests.py
@@ -1,3 +1,73 @@
 from django.test import TestCase
+from django.urls import reverse
+from rebu.models import user, eater, cook, meal, plate, eater_rating, review
 
-# Create your tests here.
+class test_user(TestCase):
+    def setUp(self):
+        pass
+
+    def test_descriptive_testname(self):
+        self.assertTrue(True)
+
+    def tearDown(self):
+        pass
+
+class test_eater(TestCase):
+    def setUp(self):
+        pass
+
+    def test_descriptive_testname(self):
+        self.assertTrue(True)
+
+    def tearDown(self):
+        pass
+
+class test_cook(TestCase):
+    def setUp(self):
+        pass
+
+    def test_descriptive_testname(self):
+        self.assertTrue(True)
+
+    def tearDown(self):
+        pass
+
+class test_meal(TestCase):
+    def setUp(self):
+        pass
+
+    def test_descriptive_testname(self):
+        self.assertTrue(True)
+
+    def tearDown(self):
+        pass
+
+class test_plate(TestCase):
+    def setUp(self):
+        pass
+
+    def test_descriptive_testname(self):
+        self.assertTrue(True)
+
+    def tearDown(self):
+        pass
+
+class test_eater_rating(TestCase):
+    def setUp(self):
+        pass
+
+    def test_descriptive_testname(self):
+        self.assertTrue(True)
+
+    def tearDown(self):
+        pass
+
+class test_review(TestCase):
+    def setUp(self):
+        pass
+
+    def test_descriptive_testname(self):
+        self.assertTrue(True)
+
+    def tearDown(self):
+        pass
