8141a4c8dabfd4064d5a060c9a56d375adace1e8
awh4kc <awh4kc@virginia.edu>
2019-02-20 22:25:32 -0500

    working tests

--- app/microservices/rebu/tests.py
+++ app/microservices/rebu/tests.py
@@ -52,7 +52,54 @@ class test_meal(TestCase):
         call_command('loaddata', 'rebu/fixtures/rebu/rebu_testdata.json', verbosity = 0)
 
     def test_verify_meal_exists(self):
-        response = self.client.get("/api/v1/meals/1/")
+        response = self.client.get(reverse('meal', args = [1]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_existing_meal(self):
+        response = self.client.get(reverse('meal', args = [2]))
+        self.assertContains(response, 'true')
+        self.assertContains(response, 'Steak Chinoise')
+
+    def test_new_meal(self):
+        data = {
+                "name": "Cheeseburger",
+                "calories": 100,
+                "description": "wild shrimp, herb-shellfish broth, saffron aioli",
+                "spice": 0,
+                "price": 2,
+                "tags": "healthy yummy food",
+                "takeout_available": "False",
+                "num_plates": 2,
+                "start": "2019-02-04T00:01:00-05:00",
+                "end": "2019-02-04T00:04:00-05:00",
+                "cook": 1
+                }
+        
+        response_post = self.client.post('/api/v1/meals/create/', data)
+        response = self.client.get('/api/v1/meals/4/')
+        self.assertContains(response_post, 'true')
+        self.assertContains(response, 'true')
+        self.assertContains(response, 'Cheeseburger')
+
+    def test_delete_meal(self):
+        response = self.client.delete('/api/v1/meals/3/')
+        self.assertContains(response, 'true')
+        response_false = self.client.get('/api/v1/meals/3/')
+        self.assertContains(response_false, 'false')
+
+    def tearDown(self):
+        pass
+
+class test_layer(TestCase):
+    def setUp(self):
+        call_command('loaddata', 'rebu/fixtures/rebu/rebu_testdata.json', verbosity = 0)
+
+    def test_all(self):
+        response = self.client.get(reverse('all'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_newest(self):
+        response = self.client.get(reverse('newest'))
         self.assertEqual(response.status_code, 200)
 
     def tearDown(self):
