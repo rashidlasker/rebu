20ae3cc08116e9b5779d7d3ae55b35d87a84f8e0
Rashid Lasker <rashidlasker@gmail.com>
2019-03-08 16:29:53 -0500

    update fixture

--- app/microservices/start-microservices.sh
+++ app/microservices/start-microservices.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 python manage.py migrate
-python manage.py flush --no-input
+python manage.py flush --no-input -v 0
 python manage.py loaddata rebu/fixtures/rebu/rebu_testdata.json 
-python manage.py collectstatic --no-input --clear
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('root', 'rashidlasker@gmail.com', 'root')" | python manage.py shell
+python manage.py collectstatic --no-input --clear -v 0
 mod_wsgi-express start-server --reload-on-changes microservices/wsgi.py
--- app/web/start-web.sh
+++ app/web/start-web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input --clear -v 0
 mod_wsgi-express start-server --reload-on-changes web/wsgi.py
