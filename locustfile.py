eefcc84ab8363a970eb1f7294256cac32869dc65
Rashid Lasker <rashidlasker@gmail.com>
2019-05-08 12:22:40 -0400

    do locust instead lmao

--- locustfile.py
+++ locustfile.py
@@ -3,7 +3,7 @@ from locust import HttpLocust, TaskSet
 def login(l):
     response = l.client.get('/login/')
     csrftoken = response.cookies['csrftoken']
-    l.client.post("/login", {"username":"yeet", "password":"password"}, headers={'X-CSRFToken': csrftoken})
+    l.client.post("/login/", {"username":"rel3en", "password":"password"}, headers={'X-CSRFToken': csrftoken})
 
 def logout(l):
     l.client.post("/logout")
@@ -18,9 +18,9 @@ def meal(l):
     l.client.get("/meals/2/")
 
 def create_meal(l):
-    response = l.client.get('/login/')
+    response = l.client.get('/meals/create')
     csrftoken = response.cookies['csrftoken']
-    l.client.post("/login", {'tags': 'asian savory meat', 'cook': 1, 'description': 'gruyere-thyme potatoes, tamari-ginger pan sauce', 'name': 'Steak Chinoise', 'calories': 100, 'price': 3.0, 'end': '2019-02-04T05:10:00Z', 'id': 2, 'start': '2019-02-04T05:06:00Z', 'num_plates': 1, 'spice': 0, 'takeout_available': False}, headers={'X-CSRFToken': csrftoken})
+    l.client.post("/meals/create", {'tags': 'asian savory meat', 'cook': 1, 'description': 'gruyere-thyme potatoes, tamari-ginger pan sauce', 'name': 'Steak Chinoise', 'calories': 100, 'price': 3.0, 'end': '2019-02-04T05:10:00Z', 'id': 2, 'start': '2019-02-04T05:06:00Z', 'num_plates': 1, 'spice': 0, 'takeout_available': False}, headers={'X-CSRFToken': csrftoken})
 
 class UserBehavior(TaskSet):
     tasks = {index: 4, search: 3, meal: 2, create_meal: 1}
