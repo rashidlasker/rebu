124862f49ac3b959f93f508467b695a22907246a
Alex <awh4kc@virginia.edu>
2019-05-08 14:40:07 -0400

    step 3

--- spark/spark.py
+++ spark/spark.py
@@ -1,15 +1,18 @@
 from pyspark import SparkContext
+from itertools import combinations
 
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
 data = sc.textFile("/tmp/data/access.log-example", 2)
 
-pairs = data.map(lambda line: line.split("\t"))
-count = pairs.groupByKey()
+pairs = data.map(lambda line: tuple(line.split("\t")))
+distinct_pairs = pairs.distinct()
+list_pairs = distinct_pairs.groupByKey()
+coview_pairs = list_pairs.flatMap(lambda pair: [[pair[0], tuple(x)] for x in combinations(list(pair[1]), 2)])
 
-output = count.collect()
+output = coview_pairs.collect()
 for page_id, ids in output:
-    print ("page_id %s list %s" % (page_id, list(ids)))
+    print ("page_id %s list %s" % (page_id, ids))
 print ("Popular items done")
 
 sc.stop()
