8963bc34f8226028e53cb80671c9b12a58d73c28
Alex <awh4kc@virginia.edu>
2019-05-08 14:00:48 -0400

    more sparki

--- spark/spark.py
+++ spark/spark.py
@@ -2,16 +2,14 @@ from pyspark import SparkContext
 
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
-data = sc.textFile("/tmp/data/access.log-example", 2)     # each worker loads a piece of the data file
+data = sc.textFile("/tmp/data/access.log-example", 2)
 
-pairs = data.map(lambda line: line.split("\t"))   # tell each worker to split each line of it's partition
-pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
-count = pages.reduceByKey(lambda x,y: int(x)+int(y))        # shuffle the data so that each key is only on one worker
-                                                  # and then reduce all the values by adding them together
+pairs = data.map(lambda line: line.split("\t"))
+count = pairs.groupByKey()
 
-output = count.collect()                          # bring the data back to the master node so we can print it out
-for page_id, count in output:
-    print ("page_id %s count %d" % (page_id, count))
+output = count.collect()
+for page_id, ids in output:
+    print ("page_id %s list %s" % (page_id, list(ids)))
 print ("Popular items done")
 
 sc.stop()
