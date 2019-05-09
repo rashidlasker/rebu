d86b76b9233a64a959910605cb029e93fdca9bde
Alex <awh4kc@virginia.edu>
2019-05-08 22:00:29 -0400

    tests

---
+++ app/microservices/rebu/tests/test_recommendation.py
@@ -0,0 +1,49 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from rebu.models import user, meal
+from urllib.parse import urlencode
+from django.core.management import call_command
+
+class test_recommendation(TestCase):
+    def setUp(self):
+        call_command('loaddata', 'rebu/fixtures/rebu/rebu_testdata.json', verbosity = 0)
+
+    def test_verify_recommendation_exists(self):
+        response = self.client.get(reverse('recommendation', args = [1]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_existing_recommendation(self):
+        response = self.client.get(reverse('recommendation', args = [2]))
+        self.assertContains(response, 'true')
+        self.assertContains(response, '1')
+
+    def test_new_meal(self):
+        data = {
+                "meal": 1,
+                "recommended_meals": "2,3"
+               }
+
+        response_post = self.client.post('/api/v1/recommendations/create/', data)
+        response = self.client.get('/api/v1/recommendations/4/')
+        self.assertContains(response_post, 'true')
+        self.assertContains(response, 'true')
+        #self.assertEqual(response.content, 'Cheeseburger')
+        self.assertContains(response, '2,3')
+        # currently failing don't know why, testing with assertEqual response.content
+
+    def test_update_meal(self):
+        data = {"recommended_meals": "2,3"}
+        response_post = self.client.post('/api/v1/recommendations/1/', data)
+        response = self.client.get('/api/v1/recommendations/1/')
+        self.assertContains(response_post, 'true')
+        self.assertContains(response, 'true')
+        self.assertContains(response, '2,3')
+
+    def test_delete_meal(self):
+        response = self.client.delete('/api/v1/recommendations/3/')
+        self.assertContains(response, 'true')
+        response_false = self.client.get('/api/v1/recommendations/3/')
+        self.assertContains(response_false, 'false')
+
+    def tearDown(self):
+        pass
