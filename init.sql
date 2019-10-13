e7e18c4f0ccde32770362e4cbe56bd4bde8ef56d
Rashid Lasker <rashidlasker@gmail.com>
2019-10-13 14:40:03 -0400

    add test db info

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
 create user IF NOT EXISTS 'www'@'%' identified with mysql_native_password by '$3cureUS';
 create database IF NOT EXISTS cs4501 character set utf8;
 grant all on cs4501.* to 'www'@'%';
+grant all on test_cs4501.* to 'www'@'%';
